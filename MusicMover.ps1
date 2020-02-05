89a99f1f5deb9c9316b6723006583c104d74fd38
feletoch <feletoch@hotmail.com>
2020-02-05 13:52:39 +1300

    Update MusicMover.ps1

--- MusicMover.ps1
+++ MusicMover.ps1
@@ -1,10 +1,18 @@
 $downloadpath = "C:\Music"
 $musicFolderPath = "D:\Music"
 $completeTimer = 90
+$fileType = "*.flac"
+    
+function Move-File(
+[Parameter(Mandatory = $true)] [string] $path
+)
+{
+            Move-Item $path -Destination $musicFolderPath -Force 
+}
 
 $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path = $downloadpath
-$watcher.Filter = "*.flac"
+$watcher.Filter = $fileType
 $watcher.IncludeSubdirectories = $true
 $watcher.EnableRaisingEvents = $true  
 
@@ -13,8 +21,14 @@ $action = {
             sleep $completeTimer
             $details = $event.SourceEventArgs
             $FullPath = $details.FullPath
-            Move-Item $FullPath -Destination $musicFolderPath -Force   
+            Move-File -path $FullPath
           }    
 
 Register-ObjectEvent $watcher "Created" -Action $action
-while ($true) {sleep 60}
+while ($true) {
+            sleep 60
+            (Get-ChildItem $downloadpath -Filter $fileType | ? {$_.LastWriteTime -lt (Get-Date).AddMinutes(-10)}) | 
+            Foreach-Object  {
+                        Move-File -path $_.FullName
+            }
+}
